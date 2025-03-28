a159b363de699711c865c471c369c92b8dfcf62c
Krzysztof Makulewski <knmekowski@gmail.com>
2025-03-28 08:17:23 +0100

    Added Sorting algorithm selection

--- Menu.py
+++ Menu.py
@@ -1,6 +1,5 @@
-from datetime import date
-
 import generators as gen
+import sorters as sort
 
 def Menu():
     T=[]
@@ -11,28 +10,30 @@ def Menu():
             case "1":
                 T = TableGenerator()
             case "2":
-                # Sorting(T)
-                print("Not ready")
+                Sorting(T)
+                # print("Not ready")
             case "3":
                 break
             case _:
                 print("Nie prawidłowy wybór")
-    print("Żegnaj")
+    print("Bye bye")
 
+#Person below me is gya
+#*gay
 
 def TableGenerator():
     while True:
         n = -1
         while n <=0:
-            n = int(input("Podaj długość tablicy: "))
+            n = int(input("Podaj długość (liczba Naturalna dodatnia) tablicy: "))
 
-        match input("Wybierz algorytm: \n 1) Losowa \n 2)Rosnąca \n 3)Malejąca \n 4)Stała \n 5) A-kształtna\n"):
+        match input("Wybierz rodzaj: \n 1) Losowa \n 2) Rosnąca \n 3) Malejąca \n 4) Stała \n 5) A-kształtna\n "):
             case "1":
                 T = gen.RandTable(n)
                 print(T)
                 return T
             case "2":
-                T = gen.IncTable
+                T = gen.IncTable(n)
                 print(T)
                 return T
             case "3":
@@ -49,3 +50,47 @@ def TableGenerator():
                 return T
             case _:
                 print("Zły wybór")
+
+def Sorting(arr):
+    while True:
+        T = [i for i in arr]
+        print("Przed sortowaniem:\n",arr)
+        match input("Wybierz algorytm: \n 1) Selection \n 2) Insertion \n 3) Bubble \n 4) Heap \n 5) Quick(lewy)\n 6) Quick(losowy) \n 7) Shell \n q) Wróć\n"):
+            case "1":
+                T = sort.selection_sort(T)
+            case "2":
+                T = sort.insertion_sort(T)
+            case "3":
+                T = sort.bubble_sort(T)
+            case "4":
+                T = sort.heap_sort(T)
+            case "5":
+                T = sort.quick_sort_left_pivot(T,0,len(T)-1)
+            case "6":
+                T = sort.quick_sort_random_pivot(T,0,len(T)-1)
+            case "7":
+                T = sort.shell_sort(T)
+            case "q":
+                break
+            case _:
+                print("Zły wybór")
+        print("Po sortowaniu:\n",T)
+
+def AlgSelect(arr,n):
+    T =[]
+    match n:
+        case "1":
+            T = sort.insertion_sort(arr)
+        case "2":
+            T = sort.shell_sort(arr)
+        case "3":
+            T = sort.selection_sort(arr)
+        case "4":
+            T = sort.heap_sort(arr)
+        case "5":
+            T = sort.quick_sort_left_pivot(arr,0,len(arr)-1)
+        case "6":
+            T = sort.quick_sort_random_pivot(arr,0,len(arr)-1)
+        case _:
+            print("Nieprawidłowa opcja")
+    return T
